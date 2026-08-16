ab289a1003bd99a383f11bdce520781b6e9992a8
Earandelix <wann00s@outlook.com>
2026-08-16 09:03:57 +0300

    Fix skill value for member Xiris

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const DISCORD = "https://discord.gg/fusuVXV6a";
 
 const members = [
   {name:"hussein", role:"CLAN LEAD", rank:"LEADER", kills:3554, skill:9.8, status:"ONLINE"},
-  {name:"Xiris", role:"CLAN LEAD", rank:"LEADER", kills:0000, skill:.10, status:"ONLINE"},
+  {name:"Xiris", role:"CLAN LEAD", rank:"LEADER", kills:0000, skill:10, status:"ONLINE"},
   {name:"PLAYER 03", role:"MEMBER", rank:"MEMBER", kills:0000, skill:7.2, status:"AWAY"},
   {name:"PLAYER 04", role:"MEMBER", rank:"MEMBER", kills:0000, skill:6.8, status:"AWAY"},
   {name:"PLAYER 05", role:"MEMBER", rank:"MEMBER", kills:0000, skill:6.6, status:"AWAY"},
