5c916d83918d581a7c7f3d3d8e19adba91356d41
Earandelix <wann00s@outlook.com>
2026-08-19 15:46:57 +0300

    Correct member kills data for 'mxessy'
    Fixed syntax error in member data for 'mxessy'.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const members = [
   {name:"Inumaki", role:"CLAN LEAD", rank:"LEADER", kills:3554, skill:9.8, status:"ONLINE"},
   {name:"Xiris", role:"CLAN LEAD", rank:"LEADER", kills:4701, skill:10, status:"ONLINE"},
   {name:"MxE", role:"moderator", rank:"MEMBER", kills:30151, skill:10, status:"online"},
-  {name:"mxessy", role:"MEMBER", rank:"MEMBER", kills9222, skill:7.2, status:"Online "},
+  {name:"mxessy", role:"MEMBER", rank:"MEMBER", kills:9222, skill:7.2, status:"Online "},
   {name:"PLAYER 05", role:"MEMBER", rank:"MEMBER", kills:0000, skil l:6.6, status:"AWAY"},
   {name:"PLAYER 06", role:"MEMBER", rank:"MEMBER", kills:0000,skill:6.1, status:"AWAY"}
 ];
